2dc59891852abe6c51c292a2bbc2b3a0fc51ee51
Noah Teshu <nteshu@gmail.com>
2015-02-21 15:17:33 -0500

    commit of first html file

--- firsttodisclose/disclosures/views.py
+++ firsttodisclose/disclosures/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.template import RequestContext
+from django.shortcuts import get_object_or_404, render_to_response
+ 
+def index(request):  
+	return render_to_response("index.html", None, RequestContext(request))
--- firsttodisclose/firsttodisclose/urls.py
+++ firsttodisclose/firsttodisclose/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from disclosures.views import index
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'firsttodisclose.views.home', name='home'),
+    url(r'^$', index),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
